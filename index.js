ee2dcb292c3a65d95a1aaba47e960bcf90c37505
Anupa <peramunaanupa@gmail.com>
2025-02-15 01:23:13 +1100

    Add start and stop buttons with event listeners
    - Add a new stop button in the popup page.
    - Implement event listeners for both start and stop buttons to send the relevant message to the background script when clicked.

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-async function lazyStreamPlay() {
-    let queryOptions = {active: true, currentWindow: true};
-    let [tab] = await chrome.tabs.query(queryOptions);
+document.getElementById("start-skip").addEventListener("click", async () => {
+    chrome.runtime.sendMessage({ action: "startSkip" });
+  });
+  
 
-    if (tab.url.includes("netflix.com/watch")) {
-        chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            files: ["netflixSkipScript.js"]
-        });
-    }
-    else {
-        alert("Please start watching a video to start the extension.");
-    }
-}
-
-document.getElementById("start-skip").addEventListener("click", lazyStreamPlay);
+  document.getElementById("stop-skip").addEventListener("click", async () => {
+    chrome.runtime.sendMessage({ action: "stopSkip" });
+  });
